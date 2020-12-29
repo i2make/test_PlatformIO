733c26a33bd878960ff7800eb8abf10e26e58b36
i2make <48208304+i2make@users.noreply.github.com>
2020-12-29 18:24:44 +0900

    Add files via upload

---
+++ cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/KJH/CLionProjects/test_PlatformIO/src/main.c" "C:/Users/KJH/CLionProjects/test_PlatformIO/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/cores/arduino"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/variants/standard"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/libraries/EEPROM/src"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/libraries/HID/src"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/libraries/SPI/src"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/libraries/SoftwareSerial/src"
+  "C:/Users/KJH/.platformio/packages/framework-arduino-avr/libraries/Wire/src"
+  "C:/Users/KJH/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include"
+  "C:/Users/KJH/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include-fixed"
+  "C:/Users/KJH/.platformio/packages/toolchain-atmelavr/avr/include"
+  "C:/Users/KJH/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
